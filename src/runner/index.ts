6e54919a2b4e4c6abbc9fda9c2d22e4006938036
Kirenai <pool_dota2@hotmail.com>
2025-05-06 18:58:07 -0500

    feat: add initial project setup with TypeScript, Prettier, and updated .gitignore

---
+++ src/runner/index.ts
@@ -0,0 +1,32 @@
+import { exec } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+
+if (!fs.existsSync(path.join(process.cwd(), 'pom.xml'))) {
+  console.error('❌ pom.xml file not found in the current directory');
+  process.exit(1);
+}
+
+const GENERATOR = process.env.GENERATOR
+
+if (!GENERATOR) {
+  console.error("⚠️ Please set the GENERATOR environment variable to the path of the generator.")
+  process.exit(1)
+}
+
+export function index() {
+  const generatorPath = path.join(GENERATOR!, "generator.jar")
+
+  exec(`bash ${generatorPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(error)
+      process.exit(1)
+    }
+
+    if (stderr) {
+      console.error(`⚠️ stderr:`, stderr)
+    }
+
+    console.log('✅ stdout:', stdout)
+  })
+}
